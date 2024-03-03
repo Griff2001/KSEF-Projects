2f744c7d7a0230784ee1ac33f642d45006fa6f55
Titus210 <tituskiplagat210@gmail.com>
2024-03-03 21:59:29 +0300

    feat: Implementation of final File. - Collection of all sensors and modules

---
+++ finalAgric.ino
@@ -0,0 +1,35 @@
+// Blynk tempelates
+#define BLYNK_TEMPLATE_ID "TMPL24u1nS5_d"
+#define BLYNK_TEMPLATE_NAME "Agriculture"
+
+// Library Imports
+#include <LiquidCrystal_I2C.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+// blynk user authentication
+
+
+
+
+#define Moisture_PIN A0
+#define PIR_PIN 
+
+// Laser Pins
+#define LReceiver D5
+#define Laser D1
+
+//
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
